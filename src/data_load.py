37f36d912b21b995ea67449ec7a430f42089d5ac
Karri Karthik <41580798+KarriKarthik@users.noreply.github.com>
2025-08-01 17:37:25 +0530

    Create data_load.py

---
+++ src/data_load.py
@@ -0,0 +1,15 @@
+from google.colab import drive
+drive.mount('/content/drive')
+"""
+The following are command line comments :
+
+!mkdir -p training_files
+!mkdir -p train_data
+!mkdir -p testing_files
+!mkdir -p test_data
+!cp /content/drive/MyDrive/oct_2017/train.zip /content/training_files/
+!cp /content/drive/MyDrive/oct_2017/test.zip /content/testing_files/
+!unzip -q /content/training_files/train.zip -d /content/train_data
+!unzip -q /content/testing_files/test.zip -d /content/test_data
+
+"""
