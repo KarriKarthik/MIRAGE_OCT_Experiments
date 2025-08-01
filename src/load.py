1df864ab3145d6fed17837f3681167d81142c3f5
Karri Karthik <41580798+KarriKarthik@users.noreply.github.com>
2025-08-01 17:46:53 +0530

    Update load.py

--- src/load.py
+++ src/load.py
@@ -1,1 +1,30 @@
+github_repo_url = "https://github.com/j-morano/MIRAGE.git"
+print(f"Cloning repository: {github_repo_url}")
+!git clone {github_repo_url}
+print("\nRepository cloned successfully!")
+!mv "MIRAGE" "MIRAGE_Updated" # Rename the folder
+!mkdir /content/MIRAGE_Updated/update_code_package
+%%writefile /content/MIRAGE_Updated/hf/__init__.py
+from huggingface_hub import PyTorchModelHubMixin
+from MIRAGE_Updated.hf.mirage_hf import MIRAGEWrapper
 
+class MIRAGEhf(MIRAGEWrapper, PyTorchModelHubMixin):
+    def __init__(
+        self,
+        input_size=512,
+        patch_size=32,
+        modalities='bscan-slo',
+        size='base',
+    ):
+        super().__init__(
+            input_size=input_size,
+            patch_size=patch_size,
+            modalities=modalities,
+            size=size,
+        )
+
+# For the MIRAGE model based on ViT-Base
+model = MIRAGEhf.from_pretrained("j-morano/MIRAGE-Base")
+# # For the MIRAGE model based on ViT-Large
+# model = MIRAGEhf.from_pretrained("j-morano/MIRAGE-Large")
+print("Model loaded successfully.")
